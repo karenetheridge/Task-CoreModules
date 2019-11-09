590179ea17d4fc1c1cb1399c31e9a2e4f312853a
Karen Etheridge <ether@cpan.org>
2019-11-08 17:15:43 -0800

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.150, github profile.

---
+++ lib/Task/CoreModules.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+package Task::CoreModules;
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
+# ABSTRACT: ...
+# KEYWORDS: ...
+
+our $VERSION = '0.001';
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Task::CoreModules;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More 0.88;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Task::CoreModules;
+
+fail('this test is TODO!');
+done_testing;
