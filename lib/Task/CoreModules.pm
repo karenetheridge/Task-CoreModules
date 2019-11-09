19fbbe4e340e380f334c368d04ade8fdb7c50939
Karen Etheridge <ether@cpan.org>
2019-11-08 17:37:59 -0800

    this may be a bad idea

--- lib/Task/CoreModules.pm
+++ lib/Task/CoreModules.pm
@@ -2,8 +2,8 @@ use strict;
 use warnings;
 package Task::CoreModules;
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
-# ABSTRACT: ...
-# KEYWORDS: ...
+# ABSTRACT: All the modules that should have been installed for your perl
+# KEYWORDS: core corelist modules missing
 
 our $VERSION = '0.001';
 
@@ -15,27 +15,25 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Task::CoreModules;
-
-    ...
+    $ cpanm Task::CoreModules
 
 =head1 DESCRIPTION
 
-...
+This is a distribution that contains no code of its own, but merely declares dependencies on a number of other
+modules: all the modules that should have been bundled with your version of Perl, but may have been omitted due to
+overly-zealous distribution package managers who decided to omit some modules to "save space".
+
+Simply declare a dependency on C<Task::CoreModules> in your project, and when installed, all core modules are
+installed. If you already have them, then no action is taken.
 
 =head1 FUNCTIONS/METHODS
 
-=head2 C<foo>
-
-...
-
-=head1 ACKNOWLEDGEMENTS
-
-...
+None.
 
 =head1 SEE ALSO
 
 =for :list
-* L<foo>
+* L<Module::CoreList> - where "core" modules are listed for each Perl version.
+
 
 =cut
--- t/01-basic.t
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-use Test::More 0.88;
-use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Task::CoreModules;
-
-fail('this test is TODO!');
-done_testing;
